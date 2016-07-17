c4a66d1f653f351bf40f4978f8aa2cb1aaeb57af
Kazuyuki Honda <hakobera@gmail.com>
2016-07-17 15:19:37 +0900

    Update examples

--- examples/example.rb
+++ examples/example.rb
@@ -1,11 +1,13 @@
 task :task1 do
-  requires :folder
   param :day, type: :time, auto_bind: true, required: true
+  requires :folder
+
   output do
     target(:google_drive_file,
           name: "test_#{day.strftime('%Y%m%d')}.txt",
           parents: input.folder_id)
   end
+
   run do
     log 'task1#run'
     output.open('w') {|f| f.puts('done') }
@@ -13,5 +15,5 @@ task :task1 do
 end
 
 task :folder, type: :google_drive_folder do
-  param_set :name, 'xyz'
+  name 'xyz'
 end
